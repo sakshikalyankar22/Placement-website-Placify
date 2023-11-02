b9656e0c9bdd4a91f9f3f5f53e95c3c9c648d315
AmeyH123 <105809100+AmeyH123@users.noreply.github.com>
2023-11-03 00:43:13 +0530

    Add files via upload

---
+++ phps/homepage/Placify/announce.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>placify.com</title>
+  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" media="screen and (max-width: 3000px)"href="style.css">
+ 
+</head>
+<body>
+  <div class="container">
+  <nav id="navbar">
+    <div id="logo">
+        <img src="placifylogo.jpg" alt="placify.com">
+    </div>
+    <ul>
+        <li class="item"><a href="#home">Overview</a></li>
+        <li class="item"><a href="#service-container">Student Login</a></li>
+        <li class="item"><a href="#client-section">HOD login</a></li>
+        <li class="item"><a href="#contact">Administrator login</a></li>
+    </ul>
+</nav>
+</div>
+</body>
+</html>
---
+++ phps/homepage/Placify/sk.php
@@ -0,0 +1,139 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Announcement</title>
+</head>
+<body>
+    <div class="container">
+        <div class="header">
+            <img id="sk" src="SGGSlogo.png" alt="">
+            <h1>Shri Guru Gobind Singhji Institute of Engineering and Technology (SGGSIET),Vishnupuri Nanded.</h1>
+        </div>
+        <table style="width: 50%;">
+            <tbody>
+                <tr style="height: 10%;">
+                    <th>Regarding Ongoing Drive 2023</th>
+                </tr>
+                <tr>
+                    <td>Microsoft in last week of Oct 2023</td>
+                </tr>
+                <tr>
+                    <td>Google in last week of Oct 2023</td>
+                </tr>
+                <tr>
+                    <td>Amazon tentative dates 9 to 14 Oct 2023</td>
+                </tr>
+                <tr>
+                    <td>Flipkart in last week of Oct 2023</td>
+                </tr>
+                <tr>
+                    <td>Endress Hauser in 1st week of Oct 2023</td>
+                </tr>
+                <tr>
+                    <td>Hexaware in 1st week of Oct 2023</td>
+                </tr>
+                <tr>
+                    <td>MyWay in last week of Oct 2023</td>
+                </tr>
+                <tr style="height: 10%;">
+                    <th>Regarding Upcoming Drive 2023</th>
+                </tr>
+                <tr>
+                    <td>Adobe in 1st week of Dec 2023</td>
+                </tr>
+                <tr>
+                    <td>Apple in last week of Dec 2023</td>
+                </tr>
+                <tr>
+                    <td>Netflix in 1st week of Jan 2024</td>
+                </tr>
+                <tr>
+                    <td>Facebook in 1st week of Feb 2024</td>
+                </tr>
+                <tr>
+                    <td>Oracle in 1st week of Dec 2024</td>
+                </tr>
+                <tr>
+                    <td>TCS in 1st week of Jan 2024</td>
+                </tr>
+                <tr>
+                    <td>Adani Groups in last week of Dec 2023</td>
+                </tr>
+                <tr>
+                    <td>Planet Spark in last week of Dec 2023 </td>
+                </tr>
+                <tr>
+                    <td>Steep Graph in 1st week of Jan 2024</td>
+                </tr>
+                <tr>
+                    <td>Coditas Internship in Dec 2023</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</body>
+</html>
+
+<style>
+    .container{
+        height: 100%;
+        background-color: rgba(255, 0, 0, 0.5);
+        background-image: url('img3.jpg');
+        background-repeat: no-repeat;
+        background-attachment: fixed;
+        background-position: center;
+        background-size: cover;
+    }
+
+    .container-opacity {
+            opacity: 0.3; /* 50% opacity applied to the whole container */
+            padding: 20px;
+        }
+
+    img{
+        width:120px ;
+        height: 90px;
+        padding:35px ;
+    }
+
+    h1{
+        color: rgb(27, 3, 3);
+        float: right;
+        margin-right: 16%;
+        margin: -130px 15px;
+        padding-top:20px ;
+        font-family: serif;
+        
+    }
+
+    .container table{
+        margin: auto;
+        border: 3px solid black;
+        padding: 10px;
+        
+    
+    }
+
+    tr:hover {
+        background-color: rgb(72, 164, 180);
+        pointer-events:visible;
+        
+    }
+
+    tbody{
+        height: 500px;
+    }
+
+    table,th,td{
+        border: 1px solid black;
+        text-align: center;
+        color:midnightblue;
+    }
+
+    table th{
+        background-image: linear-gradient(rgb(179, 107, 72), rgb(198, 177, 101), rgb(210, 164, 66));
+    }
+
+</style>
